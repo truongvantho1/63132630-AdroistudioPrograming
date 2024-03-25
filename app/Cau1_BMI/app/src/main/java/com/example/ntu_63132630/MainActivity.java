ed4d8f406e1d2b14761cac7b13ec0a7461907e86
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-03-26 00:19:08 +0700

    Hoana thanh cau 1

--- app/Cau1_BMI/app/src/main/java/com/example/ntu_63132630/MainActivity.java
+++ app/Cau1_BMI/app/src/main/java/com/example/ntu_63132630/MainActivity.java
@@ -1,26 +1,52 @@
 package com.example.ntu_63132630;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ntu_63132630.BMI.TinhBMI;
 
 public class MainActivity extends AppCompatActivity {
- String Cao, Nang;
+    EditText Cao, Nang;
+    TextView tvKetQua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-            Cao = findViewById(R.id.nhapCao);
-            Nang = findViewById(R.id.nhapcanNang);
-        });
+
+        Cao = findViewById(R.id.nhapCao);
+        Nang = findViewById(R.id.nhapcanNang);
+        tvKetQua = findViewById(R.id.tvKetQua);
+    }
+
+    public void onCaculateButtonClick(View view) {
+        double ChieuCao = 0;
+        double CanNang = 0;
+
+        if (!Cao.getText().toString().isEmpty()) {
+            ChieuCao = Double.parseDouble(Cao.getText().toString());
+        }
+
+        if (!Nang.getText().toString().isEmpty()) {
+            CanNang = Double.parseDouble(Nang.getText().toString());
+        }
+
+        TinhBMI tinhBMI = new TinhBMI();
+        double BMI = tinhBMI.TinhToanBMI(ChieuCao, CanNang);
+
+        String Ketqua;
+
+        if (BMI != -1) {
+            String BMIcat = tinhBMI.PhanLoai(BMI);
+            Ketqua = "Chỉ số BMI của bạn là: " + BMI + "\n" + "Phân loại BMI: " + BMIcat;
+        } else {
+            Ketqua = "Vui lòng nhập đúng chiều cao và cân nặng";
+        }
+
+        tvKetQua.setText(Ketqua);
     }
 }
