60d4b2678f5319889e7e792180151ececeba36ce
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-03-26 01:15:20 +0700

    cập nhật câu 3 thi giữa kì

--- Cau3_GIoiThieu/app/src/main/java/com/example/cau3_gioithieu/MainActivity.java
+++ Cau3_GIoiThieu/app/src/main/java/com/example/cau3_gioithieu/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 mydialog.create().show();
+                
             }
         });
     }
