cdc41039ee3593b0f61c2c2a0e9c57bd806aa7f2
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-04-19 13:45:09 +0700

    Update MainActivity.java

--- Baitaptuanhocsoonline/app/src/main/java/com/example/baitaptuanhocsoonline/MainActivity.java
+++ Baitaptuanhocsoonline/app/src/main/java/com/example/baitaptuanhocsoonline/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.baitaptuanhocsoonline;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -18,10 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstancegState) {
+        super.onCreate(savedInstancegState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        recyclerviewData = getRecyclerviewData();
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutManager);
+        landScapeAdapter = new LandScapeAdapter( this, recyclerviewData);
+        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
